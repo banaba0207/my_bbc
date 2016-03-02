2121ea15eaff77b1ecbd0983b0853c352628e860
banaba0207 <banaba0207@gmail.com>
2016-03-02 18:59:03 +0900

    Modify
    記事画面の編集リンクの色を変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,11 +19,14 @@
                         投稿者名: {{ $post->contributor }}　
                         投稿日時: {{ $post->created_at }}
                         @if ($post->res_id == 0)
-                        　:
+                        　
                         <a href="posts/{{ $post->post_id }}">
-                            <font size="3" color="ff0000"><b>記事詳細</b></font>
+                            <font size="3" color="1253A4"><b>記事詳細</b></font>
                         </a>
                         @endif
+                        <a href="posts/{{ $post->post_id }}/edit">
+                            <font size="3" color="1253A4"><b>編集</b></font>
+                        </a>
                     </h2>
                 </div>
                 <div class='panel-boby'>
@@ -37,11 +40,16 @@
                         <a href="media/{{ $post->fig_name}}"
                             data-lightbox="image-1" />
                         <img src="media/mini/{{ $post->fig_name}}"
-                            alt="test" height="400" width="400"/>
+                            alt="{{ $post->fig_name }}" height="400" width="400"/>
                         </a>
                     </div>
                 @endif
                 {{-- Image view end --}}
+                <div align="right">
+                <div class="btn-group">
+                {!! delete_form(['posts', $post->id]) !!}
+                </div>
+                </div>
             </div>
         </post>
     @endforeach
