bb3fd6f96fb7f11b402c1cc0f199a512db8c6765
banaba0207 <banaba0207@gmail.com>
2016-03-02 19:16:08 +0900

    Modify
    記事表示部分をone_article.blade.phpにまとめた。
    記事詳細ビュー部分で、レスにインデントをつけた。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,54 +4,8 @@
     <h1>記事一覧</h1>
     <hr/>
 
-
     @foreach($posts as $post)
-        <post>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h2 class="panel-title">
-                        【{{ $post->post_id }}】
-                        【{{ $post->res_id }}】　
-                        <a href="posts/{{ $post->post_id }}">
-                            <font size="5" color="ff0000"><b>{{ $post->title }}</b></font>
-                        </a>
-                        <br><br>
-                        投稿者名: {{ $post->contributor }}　
-                        投稿日時: {{ $post->created_at }}
-                        @if ($post->res_id == 0)
-                        　
-                        <a href="posts/{{ $post->post_id }}">
-                            <font size="3" color="1253A4"><b>記事詳細</b></font>
-                        </a>
-                        @endif
-                        <a href="posts/{{ $post->post_id }}/edit">
-                            <font size="3" color="1253A4"><b>編集</b></font>
-                        </a>
-                    </h2>
-                </div>
-                <div class='panel-boby'>
-                    <div style="padding:10px">
-                        {{ $post->body }}
-                    </div>
-                </div>
-                {{-- Image view start --}}
-                @if (!empty($post->fig_name))
-                    <div style="padding:10px">
-                        <a href="media/{{ $post->fig_name}}"
-                            data-lightbox="image-1" />
-                        <img src="media/mini/{{ $post->fig_name}}"
-                            alt="{{ $post->fig_name }}" height="400" width="400"/>
-                        </a>
-                    </div>
-                @endif
-                {{-- Image view end --}}
-                <div align="right">
-                <div class="btn-group">
-                {!! delete_form(['posts', $post->id]) !!}
-                </div>
-                </div>
-            </div>
-        </post>
+        @include('posts.one_article',['post' => $post])
     @endforeach
 @endsection
 
--- resources/views/posts/one_article.blade.php
+++ resources/views/posts/one_article.blade.php
@@ -1,13 +1,28 @@
-
 <post>
-    <div class="panel panel-default">
+    @if ($post->res_id == 0)
+        <div class="panel panel-default">
+    @else
+        <div class="panel panel-default" style="margin:0 0 0 60px">
+    @endif
         <div class="panel-heading">
             <h2 class="panel-title">
                 【{{ $post->post_id }}】
-                【{{ $post->res_id }}】　　
-                <font size="5" color="ff0000"><b>{{ $post->title }}</b></font>
-                　　投稿者名: {{ $post->contributor }}
-                　　投稿日時: {{ $post->created_at }}
+                【{{ $post->res_id }}】　
+                <a href="posts/{{ $post->post_id }}">
+                    <font size="5" color="ff0000"><b>{{ $post->title }}</b></font>
+                </a>
+                <br><br>
+                投稿者名: {{ $post->contributor }}　
+                投稿日時: {{ $post->created_at }}
+                @if ($post->res_id == 0)
+                　
+                <a href="posts/{{ $post->post_id }}">
+                    <font size="3" color="1253A4"><b>記事詳細</b></font>
+                </a>
+                @endif
+                <a href="posts/{{ $post->post_id }}/edit">
+                    <font size="3" color="1253A4"><b>編集</b></font>
+                </a>
             </h2>
         </div>
         <div class='panel-boby'>
@@ -18,20 +33,16 @@
         {{-- Image view start --}}
         @if (!empty($post->fig_name))
             <div style="padding:10px">
-                <a href="http://localhost/my_bbc/public/media/{{ $post->fig_name}}" 
-                    data-lightbox="image-1">
-                <img src="http://localhost/my_bbc/public/media/mini/{{ $post->fig_name}}"
-                    alt="test" height="400" width="400"/>
+                <a href="media/{{ $post->fig_name}}"
+                    data-lightbox="image-1" />
+                <img src="media/mini/{{ $post->fig_name}}"
+                    alt="{{ $post->fig_name }}" height="400" width="400"/>
                 </a>
             </div>
         @endif
         {{-- Image view end --}}
-
         <div align="right">
-        <div class="btn-group">
-        {!! link_to(action('PostsController@edit', [$post->id]), '編集', ['class' => 'btn btn-primary']) !!}
         {!! delete_form(['posts', $post->id]) !!}
-        </div>
         </div>
     </div>
 </post>
