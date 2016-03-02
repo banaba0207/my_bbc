2210c8b2de039fe7e88241c365bfd5a3158daddc
banaba0207 <banaba0207@gmail.com>
2016-03-02 22:11:37 +0900

    Clean
    URLを生成する部分を
    action から route に変更

--- resources/views/posts/one_article.blade.php
+++ resources/views/posts/one_article.blade.php
@@ -2,48 +2,47 @@
     @if ($post->res_id == 0)
         <div class="panel panel-default">
     @else
-        <div class="panel panel-default" style="margin:0 0 0 60px">
+        <div class="panel panel-default" style="margin:10px 0 0 60px">
     @endif
-        <div class="panel-heading">
-            <h2 class="panel-title">
-                【{{ $post->post_id }}】
-                【{{ $post->res_id }}】　
-                <a href="{{ action('PostsController@edit', $post->post_id) }}">
-                    <font size="5" color="ff0000"><b>{{ $post->title }}</b></font>
-                </a>
-                <br><br>
-                投稿者名: {{ $post->contributor }}　
-                投稿日時: {{ $post->created_at }}
-                @if ($post->res_id == 0)
-                　
-                <a href="posts/{{ $post->post_id }}">
-                    <font size="3" color="1253A4"><b>記事詳細</b></font>
-                </a>
-                @endif
-                <a href="posts/{{ $post->post_id }}/edit">
-                <a href="posts/{{ $post->post_id }}/edit">
-                    <font size="3" color="1253A4"><b>編集</b></font>
-                </a>
-            </h2>
-        </div>
-        <div class='panel-boby'>
-            <div style="padding:10px">
-                {{ $post->body }}
+            <div class="panel-heading">
+                <h2 class="panel-title">
+                    【{{ $post->post_id }}】
+                    【{{ $post->res_id }}】　
+                    <a href="{{ route('posts.show', $post->post_id) }}">
+                        <font size="5" color="ff0000"><b>{{ $post->title }}</b></font>
+                    </a>
+                    <br><br>
+                    投稿者名: {{ $post->contributor }}　
+                    投稿日時: {{ $post->created_at }}
+                    @if ($post->res_id == 0)
+                    　
+                    <a href="{{ route('posts.show', $post->post_id) }}">
+                        <font size="3" color="1253A4"><b>記事詳細</b></font>
+                    </a>
+                    @endif
+                    <a href="{{ route('posts.edit', $post->id) }}">
+                        <font size="3" color="1253A4"><b>編集</b></font>
+                    </a>
+                </h2>
+            </div>
+            <div class='panel-boby'>
+                <div style="padding:10px">
+                    {{ $post->body }}
+                </div>
+            </div>
+            {{-- Image view start --}}
+            @if (!empty($post->fig_name))
+                <div style="padding:10px">
+                    <a href="{{ asset('media/'.$post->fig_name) }}"
+                        data-lightbox="image-1" />
+                    <img src="{{ asset('media/mini/'.$post->fig_name) }}"
+                        alt="{{ $post->fig_name }}" height="400" width="400"/>
+                    </a>
+                </div>
+            @endif
+            {{-- Image view end --}}
+            <div align="right">
+            {!! delete_form(['posts', $post->id]) !!}
             </div>
         </div>
-        {{-- Image view start --}}
-        @if (!empty($post->fig_name))
-            <div style="padding:10px">
-                <a href="{{ asset('media/'.$post->fig_name) }}"
-                    data-lightbox="image-1" />
-                <img src="{{ asset('media/mini/'.$post->fig_name) }}"
-                    alt="{{ $post->fig_name }}" height="400" width="400"/>
-                </a>
-            </div>
-        @endif
-        {{-- Image view end --}}
-        <div align="right">
-        {!! delete_form(['posts', $post->id]) !!}
-        </div>
-    </div>
 </post>
