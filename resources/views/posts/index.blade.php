fa8a661da2a54db60b04c40eaf6ccca52c410677
banaba0207 <banaba0207@gmail.com>
2016-03-04 20:19:49 +0900

    Modify ページネーション
    bootstrapを使用するように変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <h1>記事一覧</h1>
+    
     <?php
+    //ページネーションの実装
     if (!empty($_GET['page'])) {
         $page = $_GET['page'];
     }else {
@@ -13,19 +15,42 @@
     }else {
         $show_num = 10;
     }
+
     $maxid = count($posts) - 1;
+    $maxpage = ceil($maxid / $show_num) - 1;
     print("posts:{$maxid}<br>");
-    for ($i = 0; $i <= ceil($maxid / $show_num) - 1; ++$i) {
-        print("<a href='?page=$i&show_num=$show_num'>");
+
+    //bootstrapのページネーションの使用準備
+    print("<div class='bs-example' data-example-id='simple-pagination'>");
+    print("<nav>");
+    print("<ul class='pagination'>");
+
+    //左端のヤジルシを page = 0 に対応させる
+    print("<li>");
+    print("<a href='?page=0&show_num=$show_num' aria-label='Previous'><span aria-hidden='true'>&laquo;</span></a>");
+    print("</li>");
+
+    //ページ番号のリンクを生成
+    for ($i = 0; $i <= $maxpage; ++$i) {
         if ($i != $page){
-            print("$i");
-        }else{
-            print("<font size='5' color='1253A4'><b>$i</b></font>");
+            print("<li><a href='?page=$i&show_num=$show_num'>$i");
+        }else{ //表示中のページ番号を大きく表示
+            print("<li class='active'><a href='?page=$i&show_num=$show_num'>$i");
         }
-        print('</a>　');
+        print('</a>');
+        print("</li>");
     }
-    ?>
-    <hr/>
+
+    //右端のヤジルシを page = maxpage に対応させる
+    print("<li>");
+    print("<a href='?page=$maxpage&show_num=$show_num' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a>");
+    print("</li>");
+    print("</ul>");
+
+    print("</nav></div>");
+    print("<hr/>");
+    //ページネーション終了
+?>
 
     @for ($i = $show_num * $page; $i < $show_num * ($page + 1); ++$i)
         <?php
